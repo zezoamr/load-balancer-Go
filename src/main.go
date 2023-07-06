968c9a0de86308fb46fdfd43099d08c49d296a52
ziad amr shalaby <zezoamr@yandex.com>
2023-07-06 03:12:32 +0300

    Revert "separate project into multiple files"
    This reverts commit 905f160e15c42fca4fee8bfbd5701009d52938e4.

--- src/main.go
+++ src/main.go
@@ -3,17 +3,91 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"./servers"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
-func main() {
-	servers := []servers.Server{
-		servers.Serverserver("https://www.bing.com"),
-		servers.newSimpleServer("https://www.google.com"),
-		servers.newSimpleServer("https://www.duckduckgo.com"),
+func handleError(err error) {
+	if err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
 	}
-	lb := loadb.newLoadBalancer("8000", servers)
+}
+
+type Server interface {
+	getAddress() string
+	isAlive() bool
+	serve(rw http.ResponseWriter, r *http.Request)
+}
+
+type SimpleServer struct {
+	addr  string
+	proxy httputil.ReverseProxy
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+}
+
+func newSimpleServer(addr string) *SimpleServer {
+	serverUrl, err := url.Parse(addr)
+	handleError(err)
+
+	return &SimpleServer{
+		addr:  addr,
+		proxy: *httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+func newLoadBalancer(port string, servers []Server) *LoadBalancer {
+
+	return &LoadBalancer{
+		port:            port,
+		servers:         servers,
+		roundRobinCount: 0,
+	}
+}
+
+func (s *SimpleServer) getAddress() string {
+	return s.addr
+}
+
+func (s *SimpleServer) isAlive() bool {
+	resp, err := http.Get("http://google.com/")
+	handleError(err)
+	return (resp.StatusCode == 200)
+}
+
+func (s *SimpleServer) serve(rw http.ResponseWriter, r *http.Request) {
+	s.proxy.ServeHTTP(rw, r)
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.isAlive() {
+		lb.roundRobinCount = lb.roundRobinCount + 1
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+	//fmt.Println("roundrobin count", lb.roundRobinCount)
+	return server
+}
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	fmt.Println("forwarding request to address: ", targetServer.getAddress())
+	targetServer.serve(rw, r)
+}
+
+func main() {
+	servers := []Server{
+		newSimpleServer("https://www.bing.com"),
+		newSimpleServer("https://www.google.com"),
+		newSimpleServer("https://www.duckduckgo.com"),
+	}
+	lb := newLoadBalancer("8000", servers)
 	handeRedirect := func(rw http.ResponseWriter, r *http.Request) {
 		lb.serveProxy(rw, r)
 	}
